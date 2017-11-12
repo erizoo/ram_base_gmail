55c4a993dbb2585e3f69c2c4fa56becd8388bf37
erizo_s <alexboiko1993@gmail.com>
2017-11-12 14:30:53 +0300

    change pop access

--- src/main/java/by/boiko/crm/service/impl/UserServiceImpl.java
+++ src/main/java/by/boiko/crm/service/impl/UserServiceImpl.java
@@ -27,7 +27,7 @@ public class UserServiceImpl implements UserService {
     private HSSFWorkbook book;
     private static final String MAIL_STORE_TYPE = "pop.gmail.com";
     private static final String USERNAME = "erizosashka@gmail.com";
-    private static final String PASSWORD = "Erizo20968";
+    private static final String PASSWORD = "Erizo209688";
     private List<Email> emailList = new ArrayList<>();
     private List<Order> orderList = new ArrayList<>();
     private List<String> nameItemList = new ArrayList<>();
