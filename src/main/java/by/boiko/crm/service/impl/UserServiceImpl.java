375edc22c6a3a3d9c0f8a71f04788349bcaf5387
erizo_s <alexboiko1993@gmail.com>
2017-11-12 15:00:48 +0300

    change pop access

--- src/main/java/by/boiko/crm/service/impl/UserServiceImpl.java
+++ src/main/java/by/boiko/crm/service/impl/UserServiceImpl.java
@@ -157,7 +157,7 @@ public class UserServiceImpl implements UserService {
             Session emailSession = Session.getDefaultInstance(properties);
 
             //create the POP3 store object and connect with the pop server
-            Store store = emailSession.getStore("pops");
+            Store store = emailSession.getStore("pop3s");
 
             store.connect(host, username, password);
 
