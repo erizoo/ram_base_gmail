68606305ac409920485b5e526ab6136ad93d1f7f
Erizo <alexboiko1993@gmail.com>
2018-03-07 14:28:26 +0300

    change field notes in configuratorForParametrs parser, now can see price for all order

--- src/main/java/by/boiko/crm/service/impl/ParserMail/PCForPrametrsParser.java
+++ src/main/java/by/boiko/crm/service/impl/ParserMail/PCForPrametrsParser.java
@@ -15,53 +15,67 @@ public class PCForPrametrsParser {
     public PCForPrametrs parser(String[] lines) {
         PCForPrametrs pcForPrametrs = new PCForPrametrs();
         for (int i = 0; i < lines.length; i++) {
-            if (lines[i].contains("Имя")){
+            if (lines[i].contains("Имя")) {
                 String[] str = lines[i + 2].split(">");
-                pcForPrametrs.setName(str[1].substring(0, str[1].length()-4));
-            } if (lines[i].contains("Мобильный")){
+                pcForPrametrs.setName(str[1].substring(0, str[1].length() - 4));
+            }
+            if (lines[i].contains("Мобильный")) {
                 String[] str = lines[i + 2].split(">");
-                pcForPrametrs.setPhoneNumber(str[1].substring(4, str[1].length()-4));
-            } if (lines[i].contains("Процессор")){
+                pcForPrametrs.setPhoneNumber(str[1].substring(4, str[1].length() - 4));
+            }
+            if (lines[i].contains("Процессор")) {
                 String[] str = lines[i + 3].split(">");
                 listSku.add("Процессор" + " " + str[2]);
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Кулер")){
+            }
+            if (lines[i].contains("Кулер")) {
                 listSku.add("Кулер");
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Материнская плата")){
+            }
+            if (lines[i].contains("Материнская плата")) {
                 listSku.add("Материнская плата");
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Видеокарта")){
+            }
+            if (lines[i].contains("Видеокарта")) {
                 listSku.add("Видеокарта" + " " + lines[i + 3].trim());
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Оперативная память")){
+            }
+            if (lines[i].contains("Оперативная память")) {
                 listSku.add("Оперативная память" + " " + lines[i + 3].trim());
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Жесткий диск")){
+            }
+            if (lines[i].contains("Жесткий диск")) {
                 listSku.add("Жесткий диск" + " " + lines[i + 3].trim());
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("SSD диск")){
+            }
+            if (lines[i].contains("SSD диск")) {
                 listSku.add("SSD диск" + " " + lines[i + 3].trim());
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Блок питания")){
+            }
+            if (lines[i].contains("Блок питания")) {
                 listSku.add("Блок питания" + " " + lines[i + 3].trim());
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Корпус")){
+            }
+            if (lines[i].contains("Корпус")) {
                 listSku.add("Корпус" + " " + lines[i + 3].trim());
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
-            } if (lines[i].contains("Оптический накопитель")){
+            }
+            if (lines[i].contains("Оптический накопитель")) {
                 listSku.add("Оптический накопитель" + " " + lines[i + 3].trim());
                 listAmount.add("1");
                 listPrice.add(lines[i + 6].trim());
+            }
+            if (lines[i].contains("Сумма заказа:")){
+                pcForPrametrs.setNotes(lines[i]);
             }
         }
         for (int i = 0; i < listSku.size(); i++) {
