91d4b8d298b82c5e2e297a97b657029a13bc327c
erizo <Alex20968>
2019-01-18 10:29:45 +0300

    - Рефакторинг Мигом

--- src/main/java/by/boiko/crm/service/impl/ParserMail/MigomParser.java
+++ src/main/java/by/boiko/crm/service/impl/ParserMail/MigomParser.java
@@ -1,5 +1,12 @@
 package by.boiko.crm.service.impl.ParserMail;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class MigomParser {
 
     public Migom parser(String[] lines) {
@@ -9,7 +16,17 @@ public class MigomParser {
                 String[] strings = lines[i].split("href");
                 String[] str = strings[1].split(">");
                 String result = str[0].substring(2).replace("\"", "");
+                Document doc = null;
+                Elements name = null;
+                try {
+                    doc = Jsoup.connect(result).get();
+                    name = doc.select("span.m-r-10");
+                    System.out.println();
+                    result = name.text();
+                } catch (IOException e) {
+                }
                 migom.setUrl(result);
+                System.out.println();
             }
             if (lines[i].contains("tel:")) {
                 String[] strings = lines[i].split(">");
